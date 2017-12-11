4ff6f073ad95b51bd6354a22e980fd93bce21d3d
Arash Keshmirian <arash@limbic.com>
2017-12-11 14:04:18 -0800

    cleanup

--- 11/11.cpp
+++ 11/11.cpp
@@ -12,57 +12,45 @@ int distance (position endpos) {
 	position pos;
 	pos.x = endpos.x;
 	pos.y = endpos.y;
-	
 	int stepcount = 0;
 	while (pos.x != 0 || pos.y != 0) {
 		if (pos.x > 0) {
-			if (pos.y > 0) {
-				//cout << "ne";
+			if (pos.y > 0) {  // ne
 				++stepcount;
 				pos.x -= 1;
 				pos.y -= 1;
-			} else if (pos.y < 0) {
-				//cout << "se";
+			} else if (pos.y < 0) {  // se
 				++stepcount;
 				pos.x -= 1;
 				pos.y += 1;
-			} else { //zigzag east
-				cout << "ZIGZAG REQUIRED, NYI. position: (" << pos.x << "," << pos.y << ")\n\n";
+			} else {  //zigzag east
 				stepcount += 2;
 				pos.x -= 2;
 			}
 		} else if (pos.x < 0) {
-			if (pos.y > 0) {
-				//cout << "nw";
+			if (pos.y > 0) {  // nw
 				++stepcount;
 				pos.x += 1;
 				pos.y -= 1;
-			} else if (pos.y < 0) {
-				//cout << "sw";
+			} else if (pos.y < 0) {  // sw
 				++stepcount;
 				pos.x += 1;
 				pos.y += 1;
-			} else {
-				cout << "ZIGZAG REQUIRED, NYI. position: (" << pos.x << "," << pos.y << ")\n\n";
+			} else {  // zigzag west
 				stepcount += 2;
 				pos.x += 2;
 			}
 		} else if (pos.x == 0) {
-			if (pos.y > 0) {
-				//cout << "n";
+			if (pos.y > 0) {  // n
 				++stepcount;
 				pos.y -= 2;
-			} else {
-				//cout << "s";
+			} else {  // s
 				++stepcount;
 				pos.y += 2;
 			}
 		}
-		//cout << ",";
 	}
-	
 	return stepcount;
-	
 }
 
 int main ()
@@ -74,33 +62,25 @@ int main ()
 		ifstream myfile("input.txt");
 		if (myfile.is_open()) {
 			while (getline(myfile,line,',')) {
-				//cout << "move: " << line << ": ";
 				if (line == "n") {
-					//cout << "north" << '\n';
 					pos.y += 2;
 				} else if (line == "ne") {
-					//cout << "north-east" << '\n';
 					pos.x += 1;
 					pos.y += 1;
 				} else if (line == "se") {
-					//cout << "south-east" << '\n';
 					pos.x += 1;
 					pos.y -= 1;
 				} else if (line == "s") {
-					//cout << "south" << '\n';
 					pos.y -= 2;
 				} else if (line == "sw") {
-					//cout << "south-west" << '\n';
 					pos.x -= 1;
 					pos.y -= 1;
 				} else if (line == "nw") {
-					//cout << "north-west" << '\n';
 					pos.x -= 1;
 					pos.y += 1;
 				}	else {
 					cout << "UNKNOWN ORDINAL" << '\n';
 				}
-				//cout << "position: (" << pos.x << "," << pos.y << ")\n\n";
 				int dist = distance(pos);
 				if (dist > maxdistance) {
 					maxdistance = dist;
@@ -111,9 +91,7 @@ int main ()
 			cout << "File open failure\n";
 			return 0;
 		}
-
-		cout << "\nmovement complete!\n" << distance(pos) << " steps. ";
-		cout << "max distance:" << maxdistance << " steps.\n";
-
+		cout << "movement complete! stats:\n   distance: " << distance(pos) << " steps. ";
+		cout << "max distance: " << maxdistance << " steps.\n";
 		return 0;
 }
